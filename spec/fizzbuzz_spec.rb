d536bc2a69e3e33eeaecc429390278071f40e387
Shemmyla Green <shemmyla@hotmail.com>
2019-04-24 04:19:56 +0000

    Automatically backed up by Learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,20 +6,28 @@ describe "fizzbuzz" do
     fizz_3 = fizzbuzz(3)
 
     expect(fizz_3).to eq("Fizz")
+  puts fizz_3
   end
+  
   it 'returns "Buzz" when the number is divisible by 5' do
     fizz_5 = fizzbuzz(5)
 
     expect(fizz_5).to eq("Buzz")
+  puts fizz_5
+  
   end
   it 'returns "FizzBuzz" when the number is divisible by 3 and 5' do
     fizz_15 = fizzbuzz(15)
 
     expect(fizz_15).to eq("FizzBuzz")
+  puts fizz_15
+  
   end
   it 'returns nil when the number is not divisible by 3 or 5' do
     fizz_4 = fizzbuzz(4)
 
     expect(fizz_4).to eq(nil)
+  puts fizz_4
+  
   end
 end
