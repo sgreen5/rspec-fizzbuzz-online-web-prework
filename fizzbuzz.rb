fc3c13d658dc015b6ec46be4c60ded7fd55f5900
Shemmyla Green <shemmyla@hotmail.com>
2019-04-24 04:53:46 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,26 +1,17 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz
-    fizz_3 = fizzbuzz(3)
-    puts fizz_3
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0
+   return "FizzBuzz"
+  end
 
+  if int % 5 == 0
+   return "Buzz"
   end
-  
-def fizzbuzz
-    fizz_5 = fizzbuzz(5)
-    puts fizz_5
+
+  if int % 3 == 0
+   return "Fizz"
   end
-  
-    def fizzbuzz
-    fizz_15 = fizzbuzz(15)
-    puts fizz_15
-  
-  end
- 
-    def fizzbuzz
-    fizz_4 = fizzbuzz(4)
-    puts fizz_4
-  
-  end
+
 end
