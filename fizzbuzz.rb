74c4997bcad0acf96d3e31719bb0477cf5f3f420
Shemmyla Green <shemmyla@hotmail.com>
2019-04-24 04:50:09 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-require_relative './spec_helper.rb'
-require_relative '../fizzbuzz.rb'
+
 def fizzbuzz
     fizz_3 = fizzbuzz(3)
     puts fizz_3
