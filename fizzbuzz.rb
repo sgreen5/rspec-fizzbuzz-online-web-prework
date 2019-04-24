322c45a71b30d4a48933770cd1de6aac86564d03
Shemmyla Green <shemmyla@hotmail.com>
2019-04-24 04:37:54 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,25 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz
+    fizz_3 = fizzbuzz(3)
+    puts fizz_3
+
+  end
+  
+def fizzbuzz
+    fizz_5 = fizzbuzz(5)
+    puts fizz_5
+  end
+  
+    def fizzbuzz
+    fizz_15 = fizzbuzz(15)
+    puts fizz_15
+  
+  end
  
+    def fizzbuzz
+    fizz_4 = fizzbuzz(4)
+    puts fizz_4
+  
+  end
 end
